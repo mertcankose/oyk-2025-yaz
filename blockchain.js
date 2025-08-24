798c67a1be3a7e53bab47def2e2224834d7043eb
Mertcan Kose <mertcankose1106@gmail.com>
2025-08-24 14:45:34 +0300

    fix

---
+++ blockchain.js
@@ -0,0 +1,79 @@
+const SHA256 = require("crypto-js/sha256");
+
+class Block {
+  constructor(index, timestamp, data, previousHash = "") {
+    this.index = index;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.hash = this.calculateHash();
+    this.nonce = 0;
+  }
+
+  calculateHash() {
+    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+  }
+
+  mineBlock(difficulty) {
+    while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+      this.nonce++;
+      this.hash = this.calculateHash();
+    }
+    console.log("Block mined: ", this.hash);
+  }
+}
+
+class Blockchain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+    this.difficulty = 5;
+  }
+
+  createGenesisBlock() {
+    return new Block(0, "16/06/2000", "Genesis Block", "0");
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    //newBlock.hash = newBlock.calculateHash();
+    newBlock.mineBlock(this.difficulty);
+    this.chain.push(newBlock);
+  }
+
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
+
+let bitcoin = new Blockchain();
+
+console.log("Mining block 1 ...");
+bitcoin.addBlock(new Block(1, "12/02/2023", { amount: 4 }));
+console.log("Mining block 2...");
+bitcoin.addBlock(new Block(2, "13/02/2023", { amount: 10 }));
+
+//----------//
+/*
+console.log("Is blockchain valid? " + bitcoin.isChainValid());
+bitcoin.chain[1].data = { amount: 100 };
+bitcoin.chain[1].hash = bitcoin.chain[1].calculateHash();
+console.log("Is blockchain valid? " + bitcoin.isChainValid());
+//console.log(JSON.stringify(bitcoin, null, 4));
+*/
