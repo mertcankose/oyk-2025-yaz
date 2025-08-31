e6c3ad6828d0e217deb29502f590746a763255b4
Mertcan Kose <mertcankose1106@gmail.com>
2025-08-31 09:06:19 +0300

    fix

--- 7-nft.sol
+++ 7-nft.sol
@@ -7,29 +7,51 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract OYKNft is ERC721, ERC721URIStorage, Ownable {
     uint256 private _nextTokenId;
-    
-    constructor(string memory name, string memory symbol) ERC721(name, symbol) Ownable(msg.sender) {
+
+    constructor(
+        string memory name,
+        string memory symbol
+    ) ERC721(name, symbol) Ownable(msg.sender) {
         _nextTokenId = 1;
     }
-    
-    function mint(address to, string memory uri) public onlyOwner returns (uint256) {
+
+    function mint(
+        address to,
+        string memory uri
+    ) public onlyOwner returns (uint256) {
         uint256 tokenId = _nextTokenId;
         _nextTokenId++;
-        
+
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
-        
+
         return tokenId;
     }
-    
+
     // Override
-    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+    function tokenURI(
+        uint256 tokenId
+    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
         return super.tokenURI(tokenId);
     }
-    
-    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721URIStorage) returns (bool) {
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC721, ERC721URIStorage) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
+
+    /*
+    function updateTokenURI(uint256 tokenId, string memory newUri) 
+        public 
+        onlyOwner 
+    {
+        require(_ownerOf(tokenId) != address(0), "Token does not exist");
+        _setTokenURI(tokenId, newUri);
+        emit MetadataUpdate(tokenId); // EIP-4906 standardı
+    }
+    */
+
     /*
     Diğer kontratlar veya dApp'ler sizin NFT'nizin hangi özellikleri desteklediğini kontrol edebilir:
     
@@ -40,5 +62,14 @@ contract OYKNft is ERC721, ERC721URIStorage, Ownable {
     if (nftContract.supportsInterface(0x5b5e139f)) { // ERC721Metadata interface ID  
         // Bu NFT metadata destekliyor
     }
+
+    Nasıl hesaplanır?
+        // Her fonksiyonun signature'ını hash'le
+        bytes4 id1 = bytes4(keccak256('balanceOf(address)'));
+        bytes4 id2 = bytes4(keccak256('ownerOf(uint256)'));
+        bytes4 id3 = bytes4(keccak256('approve(address,uint256)'));
+
+        // XOR ile birleştir
+        return id1 ^ id2 ^ id3; // ... = 0x80ac58cd
     */
 }
